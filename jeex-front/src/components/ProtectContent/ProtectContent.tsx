a27f668d289908dedcfdfdb4d5f708917eb14b60
Sergey Eroshenkov <itechmeat@gmail.com>
2023-11-19 00:58:46 +0300

    Add protected pages

--- jeex-front/src/components/ProtectContent/ProtectContent.tsx
+++ jeex-front/src/components/ProtectContent/ProtectContent.tsx
@@ -1,25 +1,30 @@
-import { FC, PropsWithChildren } from "react";
-import styles from "./ProtectContent.module.scss";
+'use client'
+import { FC, PropsWithChildren } from 'react'
+import { usePrivy } from '@privy-io/react-auth'
+import { Button } from '@radix-ui/themes'
+import styles from './ProtectContent.module.scss'
 
-type ProtectContentProps = {};
+type ProtectContentProps = {}
 
-export const ProtectContent: FC<PropsWithChildren<ProtectContentProps>> = ({
-  children,
-}) => {
-  const address = "tmp-address";
+export const ProtectContent: FC<PropsWithChildren<ProtectContentProps>> = ({ children }) => {
+  const { user, login } = usePrivy()
 
   return (
     <>
-      {!address ? (
+      {!user?.wallet?.address ? (
         <div className={styles.container}>
           <div className={styles.content}>
             <h2 className={styles.title}>Please, connect wallet</h2>
-            <div className={styles.wallet}>Wallet_BUTTON</div>
+            <div className={styles.wallet}>
+              <Button onClick={login} variant="outline">
+                Connect wallet
+              </Button>
+            </div>
           </div>
         </div>
       ) : (
         children
       )}
     </>
-  );
-};
+  )
+}
