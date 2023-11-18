f93b99ec3a333de2bd09ad93d99fb262f0779dff
Sergey Eroshenkov <itechmeat@gmail.com>
2023-11-19 02:52:38 +0300

    fix

--- jeex-front/src/features/rooms/RoomLoader/RoomLoader.tsx
+++ jeex-front/src/features/rooms/RoomLoader/RoomLoader.tsx
@@ -19,52 +19,51 @@ export const RoomLoader: FC<RoomLoaderProps> = () => {
 
   const { wsUrl } = environments
 
-  const connectToRoom = useCallback(
-    async (tkn: string) => {
-      const room = new Room()
-      const result = await room.connect(wsUrl, tkn || token)
-      setRoomOnline(room)
-    },
-    [token, wsUrl],
-  )
+  // const connectToRoom = useCallback(
+  //   async (tkn: string) => {
+  //     const room = new Room()
+  //     const result = await room.connect(wsUrl, tkn || token)
+  //     setRoomOnline(room)
+  //   },
+  //   [token, wsUrl],
+  // )
 
-  useEffect(() => {
-    ;(async () => {
-      try {
-        const resp = await fetch(
-          `/livekit/api/get-participant-token?room=${roomName}&username=${userName}`,
-        )
-        const data = await resp.json()
-        setToken(data.token)
-        connectToRoom(data.token)
-      } catch (e) {
-        console.error(e)
-      }
-    })()
-  }, [connectToRoom, roomName])
+  // useEffect(() => {
+  //   ;(async () => {
+  //     try {
+  //       const resp = await fetch(
+  //         `/livekit/api/get-participant-token?room=${roomName}&username=${userName}`,
+  //       )
+  //       const data = await resp.json()
+  //       setToken(data.token)
+  //       connectToRoom(data.token)
+  //     } catch (e) {
+  //       console.error(e)
+  //     }
+  //   })()
+  // }, [connectToRoom, roomName])
 
-  if (true) {
-    return (
-      <Article title={`Room ${roomName}`} backUrl="/rooms" isProtected>
-        <div>Room online: {roomOnline?.name}</div>
-        <Link href={`./${roomOnline?.name}/call`}>Call</Link>{' '}
-        <Link href={`./${roomOnline?.name}/edit`}>Edit</Link>
-      </Article>
-    )
-  }
-
+  // TODO: Change return with real data
   return (
     <Article title={`Room ${roomName}`} backUrl="/rooms" isProtected>
-      {token === '' ? (
-        <Loader title="Getting token..." />
-      ) : (
-        <>
-          <div>Room online: {roomOnline?.name}</div>
-          <Link href={`./${roomOnline?.name}/call`}>Call</Link>{' '}
-          <Link href={`./${roomOnline?.name}/edit`}>Edit</Link>
-          {roomOnline && <RoomConnector room={roomOnline} />}
-        </>
-      )}
+      <div>Room online: {roomOnline?.name}</div>
+      <Link href={`./${roomOnline?.name}/call`}>Call</Link>{' '}
+      <Link href={`./${roomOnline?.name}/edit`}>Edit</Link>
     </Article>
   )
+
+  // return (
+  //   <Article title={`Room ${roomName}`} backUrl="/rooms" isProtected>
+  //     {token === '' ? (
+  //       <Loader title="Getting token..." />
+  //     ) : (
+  //       <>
+  //         <div>Room online: {roomOnline?.name}</div>
+  //         <Link href={`./${roomOnline?.name}/call`}>Call</Link>{' '}
+  //         <Link href={`./${roomOnline?.name}/edit`}>Edit</Link>
+  //         {roomOnline && <RoomConnector room={roomOnline} />}
+  //       </>
+  //     )}
+  //   </Article>
+  // )
 }
