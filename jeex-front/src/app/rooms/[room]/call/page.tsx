feba2721ee0d95ffd8f4318aab4162776a36cc77
Sergey Eroshenkov <itechmeat@gmail.com>
2023-11-18 23:10:56 +0300

    Add mock of chat

--- jeex-front/src/app/rooms/[room]/call/page.tsx
+++ jeex-front/src/app/rooms/[room]/call/page.tsx
@@ -5,12 +5,15 @@ import { useParams } from 'next/navigation'
 import { Article } from '@/components/Article/Article'
 import { MeetingLayout } from '@/features/meeting/MeetingLayout/MeetingLayout'
 
+// TODO: remove this mock
+import chat from '@/mocks/chat.json'
+
 export default function RoomOnlinePage() {
   const { room: roomName } = useParams()
 
   return (
     <Article title={`Room ${roomName}`} backUrl="/rooms">
-      <MeetingLayout participants={[]} messages={[]}></MeetingLayout>
+      <MeetingLayout participants={[]} messages={chat}></MeetingLayout>
     </Article>
   )
 }
